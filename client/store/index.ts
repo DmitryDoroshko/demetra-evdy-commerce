859eb7b00da2dd3ba1180ebdec9ea715fcf34f23
Dmytro Doroshko <dmitry.doroshko.99@gmail.com>
2025-01-08 21:29:32 +0200

    Add auth feature to store.

--- client/store/index.ts
+++ client/store/index.ts
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import shoppingCart from "@/store/shopping-cart/shopping-cart.slice";
 import products from "@/store/products/products.slice";
+import auth from "@/store/auth/auth.slice";
 
 export const store = configureStore({
   reducer: {
+    auth,
     shoppingCart,
     products,
   },
