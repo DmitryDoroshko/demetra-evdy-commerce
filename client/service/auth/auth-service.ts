6cd98270c587d0b33d77ecc2f0bac3a2574218c1
Dmytro Doroshko <dmitry.doroshko.99@gmail.com>
2025-01-15 20:23:08 +0200

    Add error handling.

--- client/service/auth/auth-service.ts
+++ client/service/auth/auth-service.ts
@@ -1,5 +1,7 @@
 import http from "../http-common";
 import { ISignInBody, ISignUpBody } from "@/service/auth/auth.types";
+import { AxiosError } from "axios";
+import notification from "@/helpers/utils";
 
 class AuthService {
   private static instance: AuthService;
@@ -12,20 +14,34 @@ class AuthService {
   }
 
   async signUp(body: ISignUpBody) {
-    const response = await http.post("/users/sign-up", body);
-    return response;
+    try {
+      const response = await http.post("/users/sign-up", body);
+      return response;
+    } catch (error) {
+      const message = (error as AxiosError<{ message: string }>)?.message;
+      notification(`Error while signing up. ${message ?? ""}`, "error");
+      throw error;
+    }
   }
 
   async signIn(body: ISignInBody) {
-    const response = await http.post("/users/sign-in", body);
-    return response;
+    try {
+      const response = await http.post("/users/sign-in", body);
+      return response;
+    } catch (error) {
+      const message = (error as AxiosError<{ message: string }>)?.message;
+      notification(`Error while signing in. ${message ?? ""}`, "error");
+      throw error;
+    }
   }
 
+  // TODO: Make this get user
   async getUser() {
     const { data } = await http.get("/users/user");
     return { data };
   }
 
+  // TODO: Make this sign user out
   async signOut() {
     const { data } = await http.post("/users/sign-out");
     return { data };
